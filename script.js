7a91c90a8b25e077b6f8c595f373eff3845a81d7
Peter <46471156+fortierpm@users.noreply.github.com>
2022-03-04 12:20:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+// Try: console.log(ctx);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particlesArray = [];
+let hue = 0;
+
+// Deal with canvas resizing
+window.addEventListener("resize", (e) => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+canvas.addEventListener("click", (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+    for (let i = 0; i < 1; i++) {
+        particlesArray.push(new Particle());
+    }
+});
+canvas.addEventListener("mousemove", (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+    for (let i = 0; i < 1; i++) {
+        particlesArray.push(new Particle());
+    }
+});
+
+
+class Particle {
+    constructor() {
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.size = (Math.random() * 15) + 1;
+        this.speedX = (Math.random() * 3) - 1.5;
+        this.speedY = (Math.random() * 3) - 1.5;
+        this.color = `hsl(${hue}, 100%, 50%)`;
+        //this.color = `azure`;
+    }
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.1;
+    }
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+
+const handleParticles = () => {
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        particlesArray[i].draw();
+        for(let j = i; j < particlesArray.length; j++) {
+            const dx = particlesArray[i].x - particlesArray[j].x;
+            const dy = particlesArray[i].y - particlesArray[j].y;
+            const distance = Math.sqrt(dx**2 + dy**2); // Try with "*" instead of "+" for strange cubic(?) effect, set lineWidth to 1
+            if (distance < 100) {
+                ctx.beginPath();
+                ctx.strokeStyle = particlesArray[i].color;
+                ctx.lineWidth = 0.5;
+                //ctx.lineWidth = particlesArray[i].size / 5;
+                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+                //ctx.lineTo(particlesArray[i].x, particlesArray[j].y); Try this line for squared lines; comment out closePath below
+                ctx.lineTo(particlesArray[j].x, particlesArray[j].y)
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
+        if (particlesArray[i].size <= 0.3) {
+            particlesArray.splice(i, 1);
+            i--; // take care of shift caused by splice
+        }
+    }
+}
+
+
+const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.fillStyle = "rgba(0,0,0,0.1)";
+    //ctx.fillRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    hue += 2;
+    requestAnimationFrame(animate);
+}
+animate();
